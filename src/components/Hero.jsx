02640a86e40a2744f1617e947dc36ff7e216530e
Arshal Rejith <iamarshalrejith@gmail.com>
2025-07-23 21:31:02 +0530

    Implement Hero Scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+ 
+
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paraSplit = new SplitText(".subtitle", { type: "lines" });
@@ -12,34 +18,53 @@ const Hero = () => {
       char.classList.add("text-gradient");
     });
 
-    gsap.from(heroSplit.chars,{
-        yPercent: 100,
-        duration:1.8,
-        ease:'expo.out',
-        stagger:0.05
-    })
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.05,
+    });
 
-    
-    gsap.from(paraSplit.lines,{
-       opacity: 0,
-       yPercent:100,
-       duration:1.8,
-       ease:'expo.out',
-       stagger:0.06,
-       delay:1
-    })
+    gsap.from(paraSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+      delay: 1,
+    });
 
-    gsap.timeline({
-        scrollTrigger:{
-            trigger: '#hero',
-            start: 'top top',
-            end: 'bottom top',
-            scrub: true,
-        }
-    })
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
 
-    .to('.right-leaf',{y:200},0)
-    .to('.left-leaf',{y:-200},0)
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+
+      const startValue = isMobile ? 'top 50%' : 'center 60%'
+      const endValue = isMobile ? '120% top':'bottom top'
+
+     const tl = gsap.timeline({
+         scrollTrigger: {
+          trigger: 'video',
+          start: startValue,
+          end: endValue,
+          scrub: true,
+          pin: true
+         }
+      })
+
+      videoRef.current.onloadedmetadata = () =>{
+        tl.to(videoRef.current,{
+          currentTime: videoRef.current.duration
+        })
+      }
   }, []);
   return (
     <>
@@ -76,6 +101,10 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video ref={videoRef} src="/videos/output.mp4" playsInline muted preload="auto"></video>
+      </div>
     </>
   );
 };
