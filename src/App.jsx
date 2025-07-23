f0018362f37b08c20d099452a82ffa4fae5130f6
Arshal Rejith <iamarshalrejith@gmail.com>
2025-07-23 17:00:19 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+   <>
+   <div>
+    <h1 className='flex-center h-[100vh]'>Hello, GSAP!</h1>
+   </div>
+   </>
+  )
+}
+
+export default App
