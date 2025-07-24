d0a92b3fb2d3ad7fcbe8f67b59d6a14362c9ab36
Arshal Rejith <iamarshalrejith@gmail.com>
2025-07-24 21:54:35 +0530

    Implement of Menu section and Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import gsap from 'gsap'
-import { ScrollTrigger, SplitText } from 'gsap/all'
-import Navbar from './components/Navbar.jsx'
-import Hero from './components/Hero.jsx'
-import Cocktails from './components/Cocktails.jsx'
-import About from './components/About.jsx'
-import Art from './components/Art.jsx'
+import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
+import Menu from "./components/Menu.jsx";
+import Contact from "./components/Contact.jsx";
 
-gsap.registerPlugin(ScrollTrigger, SplitText)
+gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
     <main>
-        <Navbar />
-        <Hero />
-        <Cocktails />
-        <About />
-        <Art />
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
